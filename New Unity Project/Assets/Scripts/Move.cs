1ce2dcf4a4fa9477ce0d6f0b2d0b51894d3c88c5
Caio Nunes <caiofelipe147@gmail.com>
2017-10-21 01:45:38 -0200

    Complete move

--- New Unity Project/Assets/Scripts/Move.cs
+++ New Unity Project/Assets/Scripts/Move.cs
@@ -4,28 +4,54 @@ using UnityEngine;
 
 public class Move : MonoBehaviour {
 
-   public float maxSpeed;
+    public float maxSpeed;
+    public float minHeight;
+    public float maxHeight;
+
+    private bool canMoveUp;
+    private bool canMoveDown;
 
     void Start(){
 
     }
 
     void Update(){
-        handleMoveUp();
+        handleMoveUp(); 
     }
 
     //Handles Horizontal input and moves player
     public void handleMoveUp(){
 
         float moveUp = Input.GetAxisRaw(gameObject.GetComponent<Controles>().moveUp);
+        canMove();
 
-        if (moveUp > 0){
+        if (moveUp > 0 && canMoveUp){
             transform.Translate(0, maxSpeed * Time.deltaTime, 0);
         }
 
-        if (moveUp < 0){
+        if (moveUp < 0 && canMoveDown){
             transform.Translate(0, -maxSpeed * Time.deltaTime, 0);
         }
 
+    }
+
+    void canMove() {
+        if (gameObject.transform.position.y >= maxHeight)
+        {
+            canMoveUp = false;
+        }
+        else
+        {
+            canMoveUp = true;
+        }
+
+        if (gameObject.transform.position.y <= minHeight)
+        {
+            canMoveDown = false;
+        }
+        else
+        {
+            canMoveDown = true;
+        }
     }
 }
