68ec5fd600373598d8c0e4618c32f4f16618267f
Caio Nunes <caiofelipe147@gmail.com>
2017-10-21 17:38:11 -0200

    Rockwall

--- New Unity Project/Assets/Scripts/Magics/Rockwall.cs
+++ New Unity Project/Assets/Scripts/Magics/Rockwall.cs
@@ -6,27 +6,40 @@ public class Rockwall : MonoBehaviour {
 
     public float velocity = 2;
     public float direction;
+    public float duration;
+
+    public float positionInstante;
+
+    float cont = 0f;
+
 
     // Use this for initialization
     void Start()
     {
         if (gameObject.transform.position.x > 0)
         {
+            transform.Translate(transform.position.x - positionInstante, 0, 0);
             direction = -1;
             gameObject.GetComponent<SpriteRenderer>().flipX = true;
         }
         else
         {
             direction = 1;
+            transform.Translate(transform.position.x + positionInstante, 0, 0);
         }
-
-
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(direction * velocity * Time.deltaTime, 0, 0);
+        if (cont <= duration)
+        {
+            cont += Time.deltaTime;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
