abade2223d7872a895ebb44954e6e476fd1a8c04
Caio Nunes <caiofelipe147@gmail.com>
2017-10-22 14:32:15 -0200

    Adding new magic

--- New Unity Project/Assets/Scripts/Magics/Firebalinhas.cs
+++ New Unity Project/Assets/Scripts/Magics/Firebalinhas.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Firebalinhas : MonoBehaviour {
     public float velocity;
     public float direction;
-    float damage = 15f;
+    float damage = 0.05f;
 
     // Use this for initialization
     void Start () {
