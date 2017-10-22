da06cb5d69a4a3e05e00f61c130ff2f25ee3b99d
Caio Nunes <caiofelipe147@gmail.com>
2017-10-22 05:55:44 -0200

    oj

--- New Unity Project/Assets/Scripts/Player/Cast.cs
+++ New Unity Project/Assets/Scripts/Player/Cast.cs
@@ -67,12 +67,12 @@ public class Cast : MonoBehaviour {
 
         if (Input.GetButtonDown(this.gameObject.GetComponent<Controls>().changeElementB))
         {
-            Debug.Log(this.gameObject.name);
+            //Debug.Log(this.gameObject.name);
             ChangeElementB();
         }
 
         if (Input.GetButtonDown(this.gameObject.GetComponent<Controls>().changeElementA)){
-            Debug.Log(this.gameObject.name);
+            //Debug.Log(this.gameObject.name);
             ChangeElementA();
         }
     }
