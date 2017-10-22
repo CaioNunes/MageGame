7536afad1a296a2641bf53064402066eb78bf02e
EduardoVentura <eduardopventura@hotmail.com>
2017-10-21 23:39:28 -0200

    monte de coisa

--- New Unity Project/Assets/Scripts/Manager/LevelManager.cs
+++ New Unity Project/Assets/Scripts/Manager/LevelManager.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class LevelManager : MonoBehaviour {
+public class LevelManager : MonoBehaviour
+{
 
     public string cep1a = "ChangeElementsP1A";
     public string cep1b = "ChangeElementsP1B";
@@ -13,43 +14,21 @@ public class LevelManager : MonoBehaviour {
 
     bool sceneWinner = false;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         DontDestroyOnLoad(this);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (SceneManager.GetActiveScene().name == "MainMenu")
-        {
-            OnMainMenu();
-        }        
-        }
     }
 
-        
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
     public void LoadScene(string scene)
     {
         SceneManager.LoadScene(scene);
-    }  
-
-    void OnMainMenu()
-    {
-       if (Input.GetKeyDown("k") || Input.GetKeyDown("l") || Input.GetButtonDown(cep1a) || Input.GetButtonDown(cep1b))
-       {
-            gameObject.GetComponent<PlayerManager>().player1Ready = true;
-       }
-
-        if (Input.GetKeyDown("f") || Input.GetKeyDown("g") || Input.GetButtonDown(cep2a) || Input.GetButtonDown(cep2b))
-        {
-            gameObject.GetComponent<PlayerManager>().player2Ready = true;
-        }
-
-        if (gameObject.GetComponent<PlayerManager>().player1Ready && gameObject.GetComponent<PlayerManager>().player2Ready)
-        {
-            gameObject.GetComponent<PlayerManager>().qntPlayer = 2;
-            LoadScene("Game");
-        }
     }
-
 }
